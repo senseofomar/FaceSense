6beb7af6d049af5648226961c42a3f60ba8eb68d
Omar <senseofomar@users.noreply.github.com>
2025-12-18 14:05:22 +0530

    clean pipeline

--- src/db.py
+++ src/db.py
@@ -3,37 +3,32 @@ import MySQLdb
 
 def get_connection():
     return MySQLdb.connect(
-    host="localhost",
-    user="root",
-    passwd="facesense",
-    db="facesense",
-    port=3306
-)
+        host="localhost",
+        user="root",
+        passwd="facesense",
+        db="facesense",
+        port=3306
+    )
 
 
-def log_emotion(expression, confidence, bbox, session_id = None):
+def log_emotion(expression, confidence, bbox, session_id=None):
     try:
         conn = get_connection()
         cursor = conn.cursor()
 
-        x1, y1, x2, y2 = bbox
-
-        # Cast to native Python types
-        # - Fixed the DB error: Python type numpy.int64 cannot be converted
-        x1 = int(x1)
-        y1 = int(y1)
-        x2 = int(x2)
-        y2 = int(y2)
+        x1, y1, x2, y2 = map(int, bbox)
         confidence = float(confidence)
 
         query = """
-        INSERT INTO emotion_logs (expression, confidence, 
-                                  x1, y1, x2, y2, session_id)
+        INSERT INTO emotion_logs (expression, confidence, x1, y1, x2, y2, session_id)
         VALUES (%s, %s, %s, %s, %s, %s, %s)
         """
 
-        cursor.execute(query, (expression, confidence,
-                               x1, y1, x2, y2, session_id))
+        cursor.execute(
+            query,
+            (expression, confidence, x1, y1, x2, y2, session_id)
+        )
+
         conn.commit()
         cursor.close()
         conn.close()
