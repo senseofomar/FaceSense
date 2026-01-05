9b02b2618fc244524bedc8653e524b14588d0895
Omar <senseofomar@users.noreply.github.com>
2026-01-05 17:29:22 +0530

    cleaning the architecture

--- facesense/core/static.py
+++ facesense/core/static.py
@@ -1,5 +1,4 @@
 import cv2
-import os
 from pathlib import Path
 
 from facesense.core.face_detector import detect_faces
@@ -7,11 +6,18 @@ from facesense.core.emotion import analyze_emotion
 from facesense.storage.db import log_emotion
 
 def run_on_image(image_path, show = True, log_to_db=True):
-    img = cv2.imread(image_path)
+    image_path = Path(image_path)
+
+    if not image_path.is_absolute():
+        project_root = Path(__file__).resolve().parents[2]
+        image_path = project_root / image_path
+
+    img = cv2.imread(str(image_path))
     if img is None:
-        raise ValueError("Invalid image path")
+        raise ValueError(f"Invalid image path: {image_path}")
 
     faces = detect_faces(img)
+    print(f"Faces detected: {len(faces)}")
 
     for (x, y, w, h) in faces:
         face_roi = img[y:y+h, x:x+w]
@@ -36,22 +42,22 @@ def run_on_image(image_path, show = True, log_to_db=True):
             2
         )
 
-        # --- SAVE PROCESSED IMAGE ---
-        base_dir = Path(__file__).resolve().parents[1]
-        processed_dir = base_dir / "storage" / "processed"
-        processed_dir.mkdir(parents=True, exist_ok=True)
+    # --- SAVE PROCESSED IMAGE ---
+    base_dir = Path(__file__).resolve().parents[1]
+    processed_dir = base_dir / "storage" / "processed"
+    processed_dir.mkdir(parents=True, exist_ok=True)
 
-        input_name = Path(image_path).stem
-        output_path = processed_dir / f"{input_name}_processed.jpg"
+    input_name = Path(image_path).stem
+    output_path = processed_dir / f"{input_name}_processed.jpg"
 
-        cv2.imwrite(str(output_path), img)
-        print(f"✅ Processed image saved to: {output_path}")
+    cv2.imwrite(str(output_path), img)
+    print(f"✅ Processed image saved to: {output_path}")
 
-        # --- OPTIONAL DISPLAY ---
-        if show:
-            cv2.imshow("FaceSense – Static", img)
-            cv2.waitKey(0)
-            cv2.destroyAllWindows()
+    # --- OPTIONAL DISPLAY ---
+    if show:
+        cv2.imshow("FaceSense – Static", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
-    if __name__ == "__main__":
-        run_on_image("facesense/storage/raw/angryac.jpeg")
+if __name__ == "__main__":
+    run_on_image("facesense/storage/raw/angryac.jpeg")
