cd3b89f8b72fdcc0f670abd4e8091533fe4cb06c
Omar Shaikh <senseofomar@gmail.com>
2026-05-06 21:59:52 +0530

    emotion detection rev

--- facesense/core/emotion.py
+++ facesense/core/emotion.py
@@ -1,2 +1,13 @@
+from deepface import DeepFace
 
+def analyze_emotion(face_roi):
+    result = DeepFace.analyze(
+        face_roi,
+        actions=["emotion"],
+        enforce_detection=False
+    )
+
+    dominant = result[0]["dominant_emotion"]
+    confidence = result[0]["emotion"][dominant] / 100.0
+    return dominant, confidence
 
